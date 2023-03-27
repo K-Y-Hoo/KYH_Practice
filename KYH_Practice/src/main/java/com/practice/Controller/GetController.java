c84af5215092a795063e939d781123ac52c74c6c
K-Y-Hoo <127609484+K-Y-Hoo@users.noreply.github.com>
2023-03-27 22:37:30 +0900

    Update GetController.java

--- KYH_Practice/src/main/java/com/practice/Controller/GetController.java
+++ KYH_Practice/src/main/java/com/practice/Controller/GetController.java
@@ -10,9 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 public class GetController {
 	@GetMapping("/getParam")
-	public String getParam(@RequestParam String id, @RequestParam(name = "password") String pw) {
-		String password = "aaaa";
-		
+	public String getParam(@RequestParam String id, @RequestParam(name = "password") String pw) {		
 		System.out.println("id: " + id);
 		System.out.println("password: " + pw);
 		
